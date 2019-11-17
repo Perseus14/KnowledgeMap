30c78cc8258e28d75dcf5449b6c2b07509fe0c67
Rishabh Manoj <rishabh.manoj@iiitb.org>
2019-11-17 23:55:45 +0530

    Modified Search Screen & Result Screen

--- KnowledgeMap/clients/views.py
+++ KnowledgeMap/clients/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from .models import Client
+from .models import Client, Project
 from django.db.models import Q
 
 # Create your views here.
@@ -9,12 +9,56 @@ from django.db.models import Q
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+
 class SearchResultsView(ListView):
-    model = Client
+    model = Project
     template_name = 'search_results.html'
+
+    def or_parser(self, query):
+        flag = False
+        values = None
+        if '+' in query or '|' in query:
+            flag = True
+            values = list(map(str.lstrip, map(str.rstrip, query.split('+'))))
+            if len(values) == 1:
+                values = list(map(str.lstrip, map(str.rstrip, query.split('|'))))
+        return flag, values
+
+    def and_parser(self, query):
+        flag = False
+        values = None
+        if '&' in query:
+            flag = True
+            values = list(map(str.lstrip, map(str.rstrip, query.split('&'))))
+        return flag, values
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        return Client.objects.filter(
-            Q(name__icontains=query) | Q(desc__icontains=query)
-        )
+        print(query)
+        query = query.lstrip().rstrip()
+        flag, values = self.or_parser(query)
+        res = []
+        if flag:
+            res += [Project.objects.filter(
+                Q(tags__name__iexact=query) | Q(client__name__icontains=query)
+            ) for query in values]
+        else:
+            flag, values = self.and_parser(query)
+            if flag:
+                res += [Project.objects.filter(
+                    Q(tags__name__iexact=query) | Q(client__name__icontains=query)
+                ) for query in values]
+            else:
+                res += [Project.objects.filter(
+                    Q(tags__name__iexact=query) | Q(client__name__icontains=query)
+                )]
+
+        final_res = []
+        for res_ele in res:
+            final_res += res_ele
+
+        final_res = set(final_res)
+        print(final_res)
+        for obj in final_res:
+            print(obj.client)
+        return final_res
